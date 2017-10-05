7e032e596c711c19a598da1f14389fea36ef86f5
Rikus de Milander <rickmmmbra@gmail.com>
2017-10-05 12:19:37 +0200

    removed dangeling generated test case classes

--- core/src/main/scala/com/zenaptix/dsl/cobolClasses/Mbsk861.scala
+++
@@ -1,4 +0,0 @@
-package com.zenaptix.dsl.cobolClasses
-case class Mbsk861 (lngth861:Int, pkeyk861:Pkeyk861, datak861:Datak861)
-case class Pkeyk861 (uniqEftNoK861:String)
-case class Datak861 (incrPaymNoK861:Long, tbcK861:String)
